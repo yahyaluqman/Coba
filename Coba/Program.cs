3eee7a6b3360245c0ee3763624dfad1e9fa9461b
yahyaluqman <yahya.luqmanu21@gmail.com>
2021-11-20 14:43:24 +0700

    Update

--- Coba/Program.cs
+++ Coba/Program.cs
@@ -10,6 +10,17 @@ namespace Coba
     {
         static void Main(string[] args)
         {
+            
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+            Console.ReadLine();
+           
         }
     }
 }
